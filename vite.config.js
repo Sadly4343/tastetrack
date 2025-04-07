f9f5937f02522dfcb26898d2e35c143e9e94178c
Sadly4343 <loganhartshorn@gmail.com>
2025-04-07 02:10:36 -0600

    updates

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
+// filepath: c:\Users\Logan\tastetrack\tastetrack\vite.config.js
 import { resolve } from "path";
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
     root: "./",
+    base: "", // Use relative paths
     build: {
         outDir: './dist',
         rollupOptions: {
