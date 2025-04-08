4405a3471ccb38499eb2b803bf3c11f1b2a11b6f
Sadly4343 <loganhartshorn@gmail.com>
2025-04-08 00:31:02 -0600

    updates

---
+++ src/js/api/utils.js
@@ -0,0 +1,10 @@
+export async function loadPartial(selector, file) {
+    try {
+        const response = await fetch(file);
+        const content = await response.text();
+        document.querySelector(selector).innerHTML = content;
+
+    } catch (error) {
+        console.error(`Error loading file ${file}`, error);
+    }
+}
--- src/js/recipe.js
+++ src/js/recipe.js
@@ -1,11 +1,12 @@
 import { fetchRecipes } from "./api/recipeApi.js";
+import { loadPartial } from "./api/utils.js";
 
 async function createDisplayRecipes(recipe) {
 
     const card = document.createElement('div');
     card.classList.add('recipe-card');
-    card.innerHTML = `<img src="${recipe.image}" alt="${recipe.title}">
-            <h2>${recipe.title}</h2>`;
+    card.innerHTML = `<img src="${recipe.image}" alt="${recipe.title}" loading=lazy>
+            <h2>${recipe.title}</h2> <button >Click Here</button>`;
 
     document.querySelector('#recipe-section').appendChild(card);
 
@@ -19,6 +20,8 @@ async function displayRecipes(query) {
     });
 }
 
+loadPartial('#header-section', '/src/components/header.html')
+loadPartial('#footer-section', '/src/components/footer.html')
 
 
 
