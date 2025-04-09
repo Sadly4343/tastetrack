08bcd2a596ff36aee75925a314acd976b448cea3
Sadly4343 <loganhartshorn@gmail.com>
2025-04-08 21:13:48 -0600

    updates

--- src/js/recipe.js
+++ src/js/recipe.js
@@ -12,25 +12,25 @@ async function createDisplayRecipes(recipe) {
     cardFlipInner.classList.add('card-flip-inner');
     const card = document.createElement('div');
     card.classList.add('recipe-card');
-    card.innerHTML = `
-            <img src="${recipe.image}" alt="${recipe.title}" loading=lazy><h2>${recipe.title}</h2>
+    card.innerHTML = `<img src="${recipe.image}" alt="${recipe.title}" loading=lazy> </div>`;
+
+
+    const cardFlipBack = document.createElement('div');
+    cardFlipBack.classList.add('card-flip-back');
+    cardFlipBack.innerHTML = `<h2>${recipe.title}</h2>
             <p>Number of Ingredients: ${totalIngredients}
             <div class="likes">
             <button class="like-button" data-item-id="${recipe.id}"> ❤️ Like </button>
             <span class="like-count" >${recipe.likes}</span>
-            </div>`;
+            </div>`
 
-    const likeButton = card.querySelector(".like-button");
-    const likeCount = card.querySelector(".like-count");
 
-    const cardFlipBack = document.createElement('div');
-    cardFlipBack.classList.add('card-flip-back');
-    cardFlipBack.innerHTML = `<p>Test</p>`
-
+    const likeButton = cardFlipBack.querySelector(".like-button");
+    const likeCount = cardFlipBack.querySelector(".like-count");
     likeButton.addEventListener('click', (event) => {
         const itemId = event.target.getAttribute('data-item-id');
 
-        if (localStorage.getItem(`liked-${itemId}`)) {
+        if (localStorage.getItem(`liked - ${itemId} `)) {
             alert("You've liked this!");
             let currentLikes = parseInt(likeCount.textContent, 10);
             likeCount.textContent = currentLikes + 1;
@@ -39,7 +39,7 @@ async function createDisplayRecipes(recipe) {
             let currentLikes = parseInt(likeCount.textContent, 10);
             likeCount.textContent = currentLikes + 1;
 
-            localStorage.setItem(`liked-${itemId}`, true);
+            localStorage.setItem(`liked - ${itemId} `, true);
             alert("you liked this already");
 
         } event.target.disabled = true;
