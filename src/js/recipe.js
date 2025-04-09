8fc969a5bacdc0e0f4a77f574cdd842fbb206544
Sadly4343 <loganhartshorn@gmail.com>
2025-04-09 00:27:26 -0600

    updates

--- src/js/recipe.js
+++ src/js/recipe.js
@@ -1,4 +1,5 @@
 import { fetchRecipes } from "./api/recipeApi.js";
+import { fetchRecipeInformation } from "./api/recipeApi.js";
 import { loadPartial } from "./api/utils.js";
 
 async function createDisplayRecipes(recipe) {
@@ -22,11 +23,13 @@ async function createDisplayRecipes(recipe) {
             <div class="likes">
             <button class="like-button" data-item-id="${recipe.id}"> ❤️ Like </button>
             <span class="like-count" >${recipe.likes}</span>
+            <button type="button" class="recipeButton" data-id="${recipe.id}">Learn More</button>
             </div>`
 
 
     const likeButton = cardFlipBack.querySelector(".like-button");
     const likeCount = cardFlipBack.querySelector(".like-count");
+    const recipeButton = cardFlipBack.querySelector(".recipeButton");
     likeButton.addEventListener('click', (event) => {
         const itemId = event.target.getAttribute('data-item-id');
 
@@ -45,6 +48,18 @@ async function createDisplayRecipes(recipe) {
         } event.target.disabled = true;
     });
 
+
+
+    recipeButton.addEventListener('click', (event) => {
+        const query = event.target.getAttribute('data-id');
+        try {
+            displayRecipeInformation(query);
+        }
+        catch (error) {
+            console.error(error);
+        }
+    })
+
     cardFlipInner.appendChild(card);
     cardFlipInner.appendChild(cardFlipBack);
 
@@ -55,8 +70,25 @@ async function createDisplayRecipes(recipe) {
 
 }
 
+document.getElementById('search-button').addEventListener('click', async () => {
+    const query = document.getElementById('search').value;
+    if (!query) {
+        alert('Enter Ingredients');
+        return;
+    }
+    const container = document.getElementById('recipe-section');
+    container.innerHTML = '';
+
+    displayRecipes(query);
+})
+
+async function displayRecipeInformation(query) {
+    const recipes = await fetchRecipeInformation(query);
+    createDisplayRecipes(recipes);
+}
 
 async function displayRecipes(query) {
+
     const recipes = await fetchRecipes(query);
     recipes.forEach(recipe => {
         createDisplayRecipes(recipe);
