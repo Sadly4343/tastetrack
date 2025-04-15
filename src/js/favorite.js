90b4a66fc97578bc8025659ed02d04b0dde27065
Sadly4343 <loganhartshorn@gmail.com>
2025-04-14 21:28:01 -0600

    updates

--- src/js/favorite.js
+++ src/js/favorite.js
@@ -41,6 +41,27 @@ function createRecipe(data) {
 
 
 
+loadPartial('#header-section', '/tastetrack/src/components/header.html').then(() => {
+    const hamburgerMenu = document.querySelector("#hamburger-menu");
+    if (hamburgerMenu) {
+        console.log("hamburger menu found ")
+        hamburgerMenu.addEventListener('click', () => {
+            toggleMenu();
+        });
+    }
+});
 
-loadPartial('#header-section', '/tastetrack/src/components/header.html')
-loadPartial('#footer-section', '/tastetrack/src/components/footer.html')
+loadPartial('#footer-section', '/tastetrack/src/components/footer.html');
+
+function toggleMenu() {
+    const navMenu = document.querySelector("nav ul");
+    const hamburgerX = document.querySelector("#hamburger-x");
+    const hamburgerEqual = document.querySelector("#hamburger-equal");
+
+
+    navMenu.classList.toggle("menu-active");
+
+
+    hamburgerX.classList.toggle("hidden");
+    hamburgerEqual.classList.toggle("hidden");
+}
