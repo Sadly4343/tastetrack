2ce244e465ae3e55b136a6522a4a2080fad038f1
Sadly4343 <loganhartshorn@gmail.com>
2025-04-14 20:39:24 -0600

    Updates

--- assets/identify-B8SNxJp3.js
+++ assets/identify-B8SNxJp3.js
@@ -1,4 +1,4 @@
-import{l as n}from"./utils-D7H65duc.js";/* empty css              */document.addEventListener("DOMContentLoaded",()=>{const e=localStorage.getItem("watchlist");e&&JSON.parse(e).forEach(i=>{t(i)})});function t(e){const s=document.createElement("div");s.classList.add("card"),s.innerHTML=`<span id="closeBtn" class="close">&times;</span><h2>${e.name}</h2>
+import{l as n}from"./utils-DWjAPP_3.js";document.addEventListener("DOMContentLoaded",()=>{const e=localStorage.getItem("watchlist");e&&JSON.parse(e).forEach(i=>{t(i)})});function t(e){const s=document.createElement("div");s.classList.add("card"),s.innerHTML=`<span id="closeBtn" class="close">&times;</span><h2>${e.name}</h2>
     <img class="modal-img" src="${e.image}" alt="${e.name}">
             <div class="info-container">
                 <div class="info"><p class="time">${e.minutes}</p><p>Minutes</p></div>
